fb92b18e7a6f2699d680b3be8570f3c651b04ba4
genelim <wernminlim@gmail.com>
2015-05-05 12:12:20 +0800

    added interaction when navigate

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
 	/* Load Pages */
 	$("#education").load("education.html"); 
 	$("#experience").load("experience.html"); 
+	/*Nav Menu Scrolling Navigating */
+	$('a').each( function() {
+	    var a_tag = $(this);
+	    var target = this.hash;
+	    $(this).click(function (e) { 
+	    	if ($(target).length) {
+	    		e.preventDefault();
+	            $('html, body').animate({
+			        scrollTop: $(target).offset().top -0
+			    }, 200);  
+	    	}
+	    });
+	});  
 });
